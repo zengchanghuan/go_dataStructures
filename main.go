88d8fc32c21f30f4d3478f35343e421e257c7cb4
ZengChanghuan <zengchanghuan@gmail.com>
2021-11-01 17:12:14 +0800

    array testcast

--- main.go
+++ main.go
@@ -1,5 +1,28 @@
 package main
 
+import (
+	"fmt"
+	"go_dataStructures/ArrayList"
+)
+
 func main()  {
-	
+	array := ArrayList.NewArray()
+	array.Append("a")
+	array.Append("b")
+	array.Append("c")
+	array.Append("d")
+	array.Append("e")
+	fmt.Println(array)
+	fmt.Println(array.TheSize)
+
+}
+func main1()  {
+	array := ArrayList.NewArray()
+	array.Append(1)
+	array.Append(2)
+	array.Append(3)
+	array.Append(4)
+	array.Append(5)
+    fmt.Println(array)
+
 }
